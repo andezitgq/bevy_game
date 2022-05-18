0588f05bf8ccd4bceab6c9eab411cf6660c3560e
andezitgq <sanerhp.mail@gmail.com>
2022-05-18 15:59:38 +0300

    Orbit Camera

---
+++ src/lib/orbit_camera.rs
@@ -0,0 +1,79 @@
+use bevy::prelude::*;
+use bevy::input::mouse::*;
+
+#[derive(Component)]
+pub struct PanOrbitCamera {
+    /// The "focus point" to orbit around. It is automatically updated when panning the camera
+    pub focus: Vec3,
+    pub radius: f32,
+    pub upside_down: bool,
+}
+
+impl Default for PanOrbitCamera {
+    fn default() -> Self {
+        PanOrbitCamera {
+            focus: Vec3::ZERO,
+            radius: 5.0,
+            upside_down: false,
+        }
+    }
+}
+
+/// Pan the camera with middle mouse click, zoom with scroll wheel, orbit with right mouse click.
+pub fn pan_orbit_camera(
+    windows: Res<Windows>,
+    mut ev_motion: EventReader<MouseMotion>,
+    mut ev_scroll: EventReader<MouseWheel>,
+    input_mouse: Res<Input<MouseButton>>,
+    mut query: Query<(&mut PanOrbitCamera, &mut Transform)>,
+) {
+    let orbit_button = MouseButton::Right;
+
+    let mut rotation_move = Vec2::ZERO;
+    let mut scroll = 0.0;
+    let mut orbit_button_changed = false;
+
+    for ev in ev_motion.iter() {
+        rotation_move += ev.delta;
+    }
+        
+    for ev in ev_scroll.iter() {
+        scroll += ev.y;
+    }
+    if input_mouse.just_released(orbit_button) || input_mouse.just_pressed(orbit_button) {
+        orbit_button_changed = true;
+    }
+
+    for (mut pan_orbit, mut transform) in query.iter_mut() {
+        if orbit_button_changed {
+            let up = transform.rotation * Vec3::Y;
+            pan_orbit.upside_down = up.y <= 0.0;
+        }
+
+        if rotation_move.length_squared() > 0.0 {
+            let window = get_primary_window_size(&windows);
+            let delta_x = {
+                let delta = rotation_move.x / window.x * std::f32::consts::PI * 2.0;
+                if pan_orbit.upside_down { -delta } else { delta }
+            };
+            let delta_y = rotation_move.y / window.y * std::f32::consts::PI;
+            let yaw = Quat::from_rotation_y(-delta_x);
+            let pitch = Quat::from_rotation_x(-delta_y);
+            transform.rotation = yaw * transform.rotation; // rotate around global y axis
+            transform.rotation = transform.rotation * pitch; // rotate around local x axis
+        } else if scroll.abs() > 0.0 {
+            pan_orbit.radius -= scroll * pan_orbit.radius * 0.2;
+            pan_orbit.radius = f32::min(pan_orbit.radius, 30.0);
+            pan_orbit.radius = f32::max(pan_orbit.radius, 5.0);
+        }
+
+        let rot_matrix = Mat3::from_quat(transform.rotation);
+        transform.translation = pan_orbit.focus + rot_matrix.mul_vec3(Vec3::new(0.0, 0.0, pan_orbit.radius));
+    }
+}
+
+fn get_primary_window_size(windows: &Res<Windows>) -> Vec2 {
+    let window = windows.get_primary().unwrap();
+    let window = Vec2::new(window.width() as f32, window.height() as f32);
+    window
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
+pub mod lib;
+
 use std::f32::consts::PI as pi;
 use std::f32;
 use bevy::prelude::*;
 use bevy::window::*;
-use bevy::input::mouse::MouseMotion;
 use bevy_obj::*;
 use bevy_rapier3d::prelude::*;
 
-//Derivo de Komponantoj
-#[derive(Default)]
-struct Rotation {
-	alpha: f32,
-	beta: f32,
-}
+use lib::orbit_camera::*;
 
+//Derivo de Komponantoj
 #[derive(Component)]
 struct XP(u16);
 
@@ -33,9 +30,6 @@ struct Ground;
 
 #[derive(Component)]
 struct Coin;
-
-#[derive(Component)]
-struct PlayerEmpty;
 
 #[derive(Bundle)]
 struct PhysicsBundle {
@@ -122,7 +116,7 @@ fn main() {
 			title: "Kampludejo".to_string(),
 			resizable: true,
 			decorations: false,
-			mode: WindowMode::Windowed,
+			mode: WindowMode::BorderlessFullscreen,
 			cursor_locked: false,
 			cursor_visible: true,
 			present_mode: PresentMode::Mailbox,
@@ -133,13 +127,14 @@ fn main() {
             color: Color::WHITE,
             brightness: 1.0 / 5.0f32,
         })
-        .insert_resource(Rotation {alpha: 0.0, beta: 0.0})
 		.add_plugins(DefaultPlugins)
 		.add_plugin(ObjPlugin)
 		.add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugin(RapierDebugRenderPlugin::default())
 		.add_startup_system(setup)
+		.add_startup_system(spawn_camera)
 		.add_system(control_character)
+		.add_system(pan_orbit_camera)
 		.add_system(get_coin)
 		.run();
 }
@@ -203,22 +198,7 @@ fn setup(
 			..default()
 		},
 	});
-	
-	let empty_child = commands.spawn_bundle(TransformBundle {
-		local: Transform::from_xyz(0.0, 0.0, 0.0),
-		..default()
-	})
-	.insert(PlayerEmpty)
-	.id();
-	
-	let camera_child = commands.spawn_bundle(PerspectiveCameraBundle {
-		transform: Transform::from_xyz(0.0, 16.0, -16.0),
-        ..default()
-	})
-	.id();
-	
-	commands.entity(empty_child).push_children(&[camera_child]);
-	
+
 	//Moneroj
 	commands.spawn_bundle(CoinBundle {
 		_c: Coin,
@@ -247,20 +227,29 @@ fn setup(
 
 }
 
+pub fn spawn_camera(mut commands: Commands) {
+    let translation = Vec3::new(0.0, 16.0, -16.0);
+    let radius = translation.length();
+
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_translation(translation)
+            .looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    }).insert(PanOrbitCamera {
+        radius,
+        ..Default::default()
+    });
+}
+
 fn control_character(
 	keys: Res<Input<KeyCode>>,
-	time: Res<Time>,
-	mut rotation: ResMut<Rotation>,
-	mut camera_query: Query<&mut Transform, (With<Camera>, Without<Player>)>,
-	mut empty_query:  Query<&mut Transform, (With<PlayerEmpty>, Without<Player>, Without<Camera>)>,
+	mut camera_query: Query<(&mut PanOrbitCamera, &Transform), Without<Player>>,
 	mut player_query: Query<(&Health, &Transform, &mut ExternalForce, &mut ExternalImpulse, Entity, &mut IsGround, &Player)>,
 	mut ground_query: Query<(Entity, &Ground)>,
 	mut collision_events: EventReader<CollisionEvent>,
-	mut mouse_events: EventReader<MouseMotion>,
 ){	
 	let (_health, transform, mut _player_force, mut _player_impulse, player_ent, mut is_ground, _player) = player_query.single_mut();
-	let mut empty_transform  = empty_query.single_mut();
-	let mut camera_transform = camera_query.single_mut();
+	let (mut poc, camera_transform) = camera_query.single_mut();
 	
 	for (ground_ent, _ground) in ground_query.iter_mut() {
 		for collision_event in collision_events.iter() {
@@ -278,44 +267,19 @@ fn control_character(
 		}
 	}
 	
-	if keys.pressed(KeyCode::Up)    { _player_impulse.impulse = Vec3::new(0.0, 0.0, 1.0);}
-	if keys.pressed(KeyCode::Down)  { _player_impulse.impulse = Vec3::new(0.0, 0.0, -1.0);}
-	if keys.pressed(KeyCode::Left)  { _player_impulse.impulse = Vec3::new(1.0, 0.0, 0.0);}
-	if keys.pressed(KeyCode::Right) { _player_impulse.impulse = Vec3::new(-1.0, 0.0, 0.0);}
+	if keys.pressed(KeyCode::W) { _player_impulse.impulse = Vec3::new(0.0, 0.0, 1.0);}
+	if keys.pressed(KeyCode::S) { _player_impulse.impulse = Vec3::new(0.0, 0.0, -1.0);}
+	if keys.pressed(KeyCode::A) { _player_impulse.impulse = Vec3::new(1.0, 0.0, 0.0);}
+	if keys.pressed(KeyCode::D) { _player_impulse.impulse = Vec3::new(-1.0, 0.0, 0.0);}
 	
-	if keys.pressed(KeyCode::Up) && keys.pressed(KeyCode::Left) 	{ _player_impulse.impulse = Vec3::new(1.0, 0.0, 1.0);}
-	if keys.pressed(KeyCode::Down) && keys.pressed(KeyCode::Left)   { _player_impulse.impulse = Vec3::new(1.0, 0.0, -1.0);}
-	if keys.pressed(KeyCode::Up) && keys.pressed(KeyCode::Right)    { _player_impulse.impulse = Vec3::new(-1.0, 0.0, 1.0);}
-	if keys.pressed(KeyCode::Down) && keys.pressed(KeyCode::Right)  { _player_impulse.impulse = Vec3::new(-1.0, 0.0, -1.0);}
+	if keys.pressed(KeyCode::W) && keys.pressed(KeyCode::A) { _player_impulse.impulse = Vec3::new(1.0, 0.0, 1.0);}
+	if keys.pressed(KeyCode::S) && keys.pressed(KeyCode::A) { _player_impulse.impulse = Vec3::new(1.0, 0.0, -1.0);}
+	if keys.pressed(KeyCode::W) && keys.pressed(KeyCode::D) { _player_impulse.impulse = Vec3::new(-1.0, 0.0, 1.0);}
+	if keys.pressed(KeyCode::S) && keys.pressed(KeyCode::D) { _player_impulse.impulse = Vec3::new(-1.0, 0.0, -1.0);}
        
-    if keys.just_pressed(KeyCode::Space) && is_ground.0 == true { _player_impulse.impulse = Vec3::new(0.0, 25.0, 0.0);}			
-	
-	camera_transform.rotation = camera_transform.looking_at(Vec3::ZERO, Vec3::Z).rotation;
-	empty_transform.translation = transform.translation;
-	
-	for ev in mouse_events.iter() {
-		rotation.alpha += radian(ev.delta.y);
-		rotation.beta  += radian(ev.delta.x);
-				
-		/*empty_transform.rotate(Quat::from_axis_angle(Vec3::new(
-		
-												f32::cos(rotation.alpha),
-												0.0,
-												f32::sqrt(1.0 - (f32::cos(rotation.alpha)*f32::cos(rotation.alpha))),
-		
-																), radian(ev.delta.y)));   //ŝanĝo laŭ X
-																
-		empty_transform.rotate(Quat::from_axis_angle(Vec3::new(
-		
-												0.0,
-												f32::cos(rotation.beta),
-												f32::sqrt(1.0 - (f32::cos(rotation.beta)*f32::cos(rotation.beta))),
-		
-																), radian(-ev.delta.x)));	  //ŝanĝo laŭ Y*/
-																
-		empty_transform.rotate(Quat::from_axis_angle(Vec3::new(0.0, 1.0, 0.0), radian(-ev.delta.x)));
-		
-    }
+    if keys.just_pressed(KeyCode::Space) && is_ground.0 == true { _player_impulse.impulse = Vec3::new(0.0, 25.0, 0.0);}
+    
+    poc.focus = transform.translation;
 }
 
 fn get_coin(
@@ -367,4 +331,3 @@ fn _print_type<T>(_: &T) {
 fn radian(deg: f32) -> f32 {
 	return deg * pi / 180.0; 
 }
-
