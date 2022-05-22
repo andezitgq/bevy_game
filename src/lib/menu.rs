f1c21b66355a01a55f474ec71badc18f197a4bd1
andezitgq <sanerhp.mail@gmail.com>
2022-05-22 16:45:01 +0300

    egui ĉefmenuo

--- src/lib/menu.rs
+++ src/lib/menu.rs
@@ -27,12 +27,29 @@ pub fn main_menu(
 	mut egui_ctx: ResMut<EguiContext>,
 	mut commands: Commands,
 	mut exit: EventWriter<AppExit>,
-	keys: Res<Input<KeyCode>>,
 ){
-	 egui::CentralPanel::default().show(egui_ctx.ctx_mut(), |ui| {
-        ui.with_layout(egui::Layout::centered_and_justified(egui::Direction::TopDown), |ui| {
+	let mut fonts = egui::FontDefinitions::default();
+		
+	fonts.font_data.insert("my_font".to_owned(),
+	egui::FontData::from_static(include_bytes!("../../assets/fonts/ubuntu.ttf"))); 
+
+	fonts.families.get_mut(&egui::FontFamily::Proportional).unwrap()
+		.insert(0, "my_font".to_owned());
+
+	fonts.families.get_mut(&egui::FontFamily::Monospace).unwrap()
+		.push("my_font".to_owned());
+
+	egui_ctx.ctx_mut().set_fonts(fonts);
+	
+	egui::SidePanel::left("side_panel").default_width(200.0).show(egui_ctx.ctx_mut(), |ui| {
+        ui.vertical_centered(|ui| {
+			ui.allocate_space(egui::Vec2::new(1.0, 300.0));
 			ui.heading("Kampludejo");
 			if ui.button("Ludi").clicked() {
+				commands.insert_resource(NextState(GameState::InGame));
+			}
+			
+			if ui.button("Eliri").clicked() {
 				exit.send(AppExit);
 			}
         });
@@ -44,8 +61,4 @@ pub fn main_menu(
             ));
         });
     });
-    
-    if keys.pressed(KeyCode::Space) {
-		commands.insert_resource(NextState(GameState::InGame));
-	}
 }
